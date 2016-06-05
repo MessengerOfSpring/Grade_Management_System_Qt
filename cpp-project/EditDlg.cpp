aa4b9795ac5461243581e0a493cd6a8731ebfc5f
梁鹏 <tcx4c70@zju.edu.cn>
2016-06-05 21:31:02 +0800

    add EditDlg

--- cpp-project/EditDlg.cpp
+++ cpp-project/EditDlg.cpp
@@ -1,14 +1,47 @@
+#include "Grade.h"
 #include "EditDlg.h"
 #include "ui_EditDlg.h"
+#include <QMessageBox>
 
-EditDlg::EditDlg(QWidget *parent) :
+EditDlg::EditDlg(const QString& StudentName, const QString& CourseName, QWidget *parent) :
     QDialog(parent),
     ui(new Ui::EditDlg)
 {
     ui->setupUi(this);
+    setWindowTitle("编辑成绩");
+    ui->label->setText("请输入" + StudentName + "同学在" + CourseName + "课程中新的成绩:");
+    ui->label->adjustSize();
 }
 
 EditDlg::~EditDlg()
 {
     delete ui;
+}
+
+//--------------------------------------------------------------------------------------
+//       Class:  EditDlg
+//    Accessor:  getNewScore
+//--------------------------------------------------------------------------------------
+unsigned int EditDlg::getNewScore() const
+{
+    return m_NewScore;
+}		// -----  end of accessor EditDlg::getNewScore  ----- 
+
+void EditDlg::on_Ok_clicked()
+{
+    bool Ok;
+    m_NewScore = ui->NewScore->text().toUInt(&Ok, 10);
+    if(!Ok || m_NewScore > 100)
+    {
+        QMessageBox::warning(this, "警告", "成绩必须为0～100的整数！请重新输入");
+        ui->NewScore->clear();
+        return;
+    }
+    else
+        accept();
+}
+
+void EditDlg::on_Cancel_clicked()
+{
+    close();
 }
--- cpp-project/EditDlg.h
+++ cpp-project/EditDlg.h
@@ -1,6 +1,7 @@
 #ifndef EDITDLG_H
 #define EDITDLG_H
 
+#include "Grade.h"
 #include <QDialog>
 
 namespace Ui {
@@ -12,11 +13,17 @@ class EditDlg : public QDialog
     Q_OBJECT
 
 public:
-    explicit EditDlg(QWidget *parent = 0);
+    explicit EditDlg(const QString& StudentName, const QString& CourseName, QWidget *parent = 0);
     ~EditDlg();
+    unsigned int getNewScore() const;
+
+private slots:
+    void on_Ok_clicked();
+    void on_Cancel_clicked();
 
 private:
     Ui::EditDlg *ui;
+    unsigned int m_NewScore;
 };
 
 #endif // EDITDLG_H
