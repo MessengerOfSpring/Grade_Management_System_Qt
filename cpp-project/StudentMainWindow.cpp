3f5676d34a3486ac2c1278b2c4194459f5b15e23
梁鹏 <tcx4c70@zju.edu.cn>
2016-06-05 21:39:09 +0800

    modify student mainwindow

--- cpp-project/StudentMainWindow.cpp
+++ cpp-project/StudentMainWindow.cpp
@@ -1,8 +1,10 @@
 #include <QDateTime>
+#include <vector>
+#include "Grade.h"
 #include "StudentMainWindow.h"
 #include "ui_StudentMainWindow.h"
 
-StudentMainWindow::StudentMainWindow(QString ID, QString Password, QWidget *parent) :
+StudentMainWindow::StudentMainWindow(QString ID, QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::StudentMainWindow)
 {
@@ -21,16 +23,59 @@ StudentMainWindow::StudentMainWindow(QString ID, QString Password, QWidget *pare
     ui->tableWidget->horizontalHeader()->setSectionsClickable(false); // horizontal header can't be clicked
     ui->tableWidget->setSelectionBehavior(QAbstractItemView::SelectRows); // set selection only rows
 
-    QString LabelInfo = ID + QString("，欢迎来到学生成绩管理系统！今天是") +
-            QDate::currentDate().toString(Qt::LocalDate);
+    QString LabelInfo = ID + QString("同学，欢迎来到学生成绩管理系统！今天是") +
+            QDate::currentDate().toString("yyyy-MM-dd");
     ui->label->setText(LabelInfo);
     ui->label->adjustSize();
+
+    m_Grade = new Grade;
+    Condition Condition1 = {STUDENTID, EQUAL, ID}, Condition2 = {STUDENTID, EQUAL, ID};
+    ConditionRelation CRelation = AND;
+    QString ErrorMsg;
+    m_Grade->query(Condition1, Condition2, CRelation, ErrorMsg);
+
+    showGradeInfo();
 }
 
 StudentMainWindow::~StudentMainWindow()
 {
     delete ui;
+    delete m_Grade;
 }
+
+//--------------------------------------------------------------------------------------
+//       Class:  StudentMainWindow
+//      Method:  showGradeInfo
+// Description:  Show CourseID, CourseName, and Score
+//--------------------------------------------------------------------------------------
+void StudentMainWindow::showGradeInfo()
+{
+    // Remove all rows in the table
+    for(int Row = ui->tableWidget->rowCount() - 1; Row >= 0; Row --)
+        ui->tableWidget->removeRow(0);
+
+    const std::vector<GradeInfo>& Info = m_Grade->getInfo();
+    for(unsigned int Row = 0; Row < Info.size(); Row ++)
+    {
+        // Insert one row into the table
+        ui->tableWidget->insertRow(Row);
+
+        // Insert CourseID into the table
+        QTableWidgetItem *CourseID = new QTableWidgetItem(Info[Row].CourseID);
+        CourseID->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+        ui->tableWidget->setItem(Row, 0, CourseID);
+
+        // Insert CourseName into the table
+        QTableWidgetItem *CourseName = new QTableWidgetItem(Info[Row].CourseName);
+        CourseName->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+        ui->tableWidget->setItem(Row, 1, CourseName);
+
+        // Insert the Score into the table
+        QTableWidgetItem *Score = new QTableWidgetItem(QString("%1").arg(Info[Row].Score));
+        Score->setTextAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
+        ui->tableWidget->setItem(Row, 2, Score);
+    }
+}		// -----  end of method StudentMainWindow::showGradeInfo  ----- 
 
 void StudentMainWindow::on_Quit_clicked()
 {
--- cpp-project/StudentMainWindow.h
+++ cpp-project/StudentMainWindow.h
@@ -1,6 +1,8 @@
 #ifndef STUDENTMAINWINDOW_H
 #define STUDENTMAINWINDOW_H
 
+#include "Grade.h"
+#include <vector>
 #include <QMainWindow>
 
 namespace Ui {
@@ -12,14 +14,16 @@ class StudentMainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    explicit StudentMainWindow(QString ID, QString Password, QWidget *parent = 0);
+    explicit StudentMainWindow(QString ID, QWidget *parent = 0);
     ~StudentMainWindow();
+    void showGradeInfo();
 
 private slots:
     void on_Quit_clicked();
 
 private:
     Ui::StudentMainWindow *ui;
+    Grade *m_Grade;
 };
 
 #endif // STUDENTMAINWINDOW_H
