c88ea42d43071e87d0f7fb7ea3a5e838c7a8a27c
梁鹏 <tcx4c70@zju.edu.cn>
2016-06-05 21:31:51 +0800

    add FindDlg

--- cpp-project/FindDlg.cpp
+++ cpp-project/FindDlg.cpp
@@ -6,9 +6,97 @@ FindDlg::FindDlg(QWidget *parent) :
     ui(new Ui::FindDlg)
 {
     ui->setupUi(this);
+
+    setWindowTitle("查找成绩");
+
+    QStringList Fields, FieldRelations;
+    Fields << tr("学号") << tr("课程名称") << tr("课程代码");
+    FieldRelations << tr("等于") << tr("包含");
+    ui->Field1->addItems(Fields);
+    ui->Field2->addItems(Fields);
+    ui->FieldRelation1->addItems(FieldRelations);
+    ui->FieldRelation2->addItems(FieldRelations);
+
+    ui->And->setChecked(true);
+    m_CRelation = AND;
 }
 
 FindDlg::~FindDlg()
 {
     delete ui;
+}
+
+//--------------------------------------------------------------------------------------
+//       Class:  FindDlg
+//    Accessor:  getCondition1
+//--------------------------------------------------------------------------------------
+const Condition& FindDlg::getCondition1() const
+{
+	return m_Condition1;
+}		// -----  end of accessor FindDlg::getCondition1  ----- 
+
+//--------------------------------------------------------------------------------------
+//       Class:  FindDlg
+//    Accessor:  getCondition2
+//--------------------------------------------------------------------------------------
+const Condition& FindDlg::getCondition2() const
+{
+	return m_Condition2;
+}		// -----  end of accessor FindDlg::getCondition2  ----- 
+
+//--------------------------------------------------------------------------------------
+//       Class:  FindDlg
+//    Accessor:  getConditionRelation
+//--------------------------------------------------------------------------------------
+const ConditionRelation& FindDlg::getConditionRelation() const
+{
+	return m_CRelation;
+}		// -----  end of accessor FindDlg::getConditionRelation  ----- 
+
+void FindDlg::on_OK_clicked()
+{
+    // get information of Condition1
+    switch(ui->Field1->currentIndex())
+    {
+        case 0: m_Condition1.Field = STUDENTID; break;
+        case 1: m_Condition1.Field = COURSENAME; break;
+        case 2: m_Condition1.Field = COURSEID; break;
+    }
+    switch(ui->FieldRelation1->currentIndex())
+    {
+        case 0: m_Condition1.Relation = EQUAL; break;
+        case 1: m_Condition1.Relation = CONTAIN; break;
+    }
+    m_Condition1.Key = ui->Key1->text();
+
+    // get information of Condition2
+    switch(ui->Field2->currentIndex())
+    {
+        case 0: m_Condition2.Field = STUDENTID; break;
+        case 1: m_Condition2.Field = COURSENAME; break;
+        case 2: m_Condition2.Field = COURSEID; break;
+    }
+    switch(ui->FieldRelation2->currentIndex())
+    {
+        case 0: m_Condition2.Relation = EQUAL; break;
+        case 1: m_Condition2.Relation = CONTAIN; break;
+    }
+    m_Condition2.Key = ui->Key2->text();
+
+    accept();
+}
+
+void FindDlg::on_Cancel_clicked()
+{
+    close();
+}
+
+void FindDlg::on_And_clicked()
+{
+    m_CRelation = AND;
+}
+
+void FindDlg::on_Or_clicked()
+{
+    m_CRelation = OR;
 }
--- cpp-project/FindDlg.h
+++ cpp-project/FindDlg.h
@@ -1,6 +1,7 @@
 #ifndef FINDDLG_H
 #define FINDDLG_H
 
+#include "Grade.h"
 #include <QDialog>
 
 namespace Ui {
@@ -14,9 +15,21 @@ class FindDlg : public QDialog
 public:
     explicit FindDlg(QWidget *parent = 0);
     ~FindDlg();
+    const Condition& getCondition1() const;
+    const Condition& getCondition2() const;
+    const ConditionRelation& getConditionRelation() const;
+
+private slots:
+    void on_OK_clicked();
+    void on_Cancel_clicked();
+    void on_And_clicked();
+    void on_Or_clicked();
 
 private:
     Ui::FindDlg *ui;
+    Condition m_Condition1;
+    Condition m_Condition2;
+    ConditionRelation m_CRelation;
 };
 
 #endif // FINDDLG_H
