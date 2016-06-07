529b8376f91e4bb2b49815e963b8d034d0b47956
R11happy <xushuai100@126.com>
2016-06-07 11:24:20 +0800

    add AdminDlg

---
+++ cpp-project/AdminDlg.cpp
@@ -0,0 +1,82 @@
+﻿// =====================================================================================
+//
+//       Filename:  AdminDlg.cpp
+//
+//    Description:  Enroll for admin
+//
+//        Version:  1.0
+//        Created:  07/06/2016 09:58
+//       Revision:  none
+//       Compiler:  g++
+//
+//         Author:  R11happy, xushuai100@126.com
+//   Organization:
+//
+// =====================================================================================
+#include "AdminDlg.h"
+#include "ui_AdminDlg.h"
+#include <QMessageBox>
+#include<QSqlDatabase>
+#include <QSqlQuery>
+#include <QString>
+
+
+AdminDlg::AdminDlg(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::AdminDlg)
+{
+    ui->setupUi(this);
+    setWindowTitle(QString::fromLocal8Bit("管理员注册"));
+    setWindowIcon(QIcon(":/Menu/images/admin.png"));
+    connect(ui->admin_ok, SIGNAL(clicked(bool)),this,SLOT(on_btnOK()));
+    connect(ui->admin_cancel,SIGNAL(clicked(bool)),this,SLOT(on_btnCancel()));
+}
+
+AdminDlg::~AdminDlg()
+{
+    delete ui;
+}
+
+
+// ===  FUNCTION  ======================================================================
+//         Name:  on_btnOK
+//  Description:  insert (ID,Password, name) into Table(admin)
+// =====================================================================================
+void AdminDlg::on_btnOK()
+{
+    QString ID = ui->admin_id->text();
+    QString Password = ui->admin_psw->text();
+    QString Name = ui->admin_name->text();
+    QSqlQuery Admin_query;
+    if(ID.isEmpty())
+    {
+        QMessageBox::information(this,QString::fromLocal8Bit("提示"),QString::fromLocal8Bit("账号不能为空"));
+        return;
+    }
+    else if(Password.isEmpty())
+    {
+        QMessageBox::information(this,QString::fromLocal8Bit("提示"),QString::fromLocal8Bit("密码不能为空"));
+        return;
+    }
+    else if(Name.isEmpty())
+    {
+        QMessageBox::information(this,QString::fromLocal8Bit("提示"),QString::fromLocal8Bit("名称不能为空"));
+        return;
+    }
+    else if(!Admin_query.exec("insert into admin values('"+ID+"','"+Password+"','"+Name+"');"))
+    {
+        QMessageBox::information(this,QString::fromLocal8Bit("提示"),QString::fromLocal8Bit("注册失败"));
+        return;
+     }
+    else
+    {
+        QMessageBox::information(this,QString::fromLocal8Bit("提示"),QString::fromLocal8Bit("注册成功"));
+        accept();
+    }
+
+}
+
+void AdminDlg::on_btnCancel()
+{
+    reject();
+}
---
+++ cpp-project/AdminDlg.h
@@ -0,0 +1,24 @@
+﻿#ifndef ADMINDLG_H
+#define ADMINDLG_H
+
+#include <QDialog>
+
+namespace Ui {
+class AdminDlg;
+}
+
+class AdminDlg : public QDialog
+{
+    Q_OBJECT
+private slots:
+    void on_btnOK();
+    void on_btnCancel();
+public:
+    explicit AdminDlg(QWidget *parent = 0);
+    ~AdminDlg();
+
+private:
+    Ui::AdminDlg *ui;
+};
+
+#endif // ADMINDLG_H
